e1ec0b98d08914599292b40f571071174c63e0cf
Ensiye <ensiye.karimi1362@gmail.com>
2023-05-27 14:23:13 +0330

    correct css file address

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
-import './styles/`default`.css'
+import './styles/default.css'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 
